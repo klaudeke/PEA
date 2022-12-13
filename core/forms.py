f8d00ff6b26726b086d936203d769c8472fe529d
Gonzalo <go.cabargas@duocuc.cl>
2022-12-13 09:58:06 -0300

    se completa agregar instructor

--- core/forms.py
+++ core/forms.py
@@ -4,6 +4,13 @@ from .models import Instructor
 
 class InstructorForm(forms.ModelForm):
 
+    # nombre = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+    # email = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+    # celular = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+    # runInstructor = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+    # genero = forms.IntegerField(widget=forms.CheckboxInput(attrs={"class":"dropdown-menu"}))
+    # certificado = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+
     class Meta:
         model = Instructor
         #fields = ["nombre","email","celular","runInstructor","genero","certificado"]
